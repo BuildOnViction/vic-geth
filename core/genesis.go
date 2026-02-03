fcfb4cdaeadd392e5d15e10b45b863fea5dc6ef1
vienlnn <151703621+vienlnn@users.noreply.github.com>
2026-02-03 11:00:44 +0700

    Update core/genesis.go
    Co-authored-by: imduchuyyy 🐬 <duchuy.124dk@gmail.com>

--- core/genesis.go
+++ core/genesis.go
@@ -158,8 +158,6 @@ func SetupGenesisBlock(db ethdb.Database, genesis *Genesis) (*params.ChainConfig
 	}
 	// Just commit the new block if there is no stored genesis block.
 	stored := rawdb.ReadCanonicalHash(db, 0)
-	log.Debug("SetupGenesisBlock: checking stored genesis", "stored", stored.Hex(), "empty", stored == common.Hash{})
-
 	if (stored == common.Hash{}) {
 		if genesis == nil {
 			log.Info("Writing default main-net genesis block")
