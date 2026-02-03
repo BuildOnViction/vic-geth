cd59d0b621023ee3a35fbf679207522b025110a4
imduchuyyy <duchuy.124dk@gmail.com>
2026-02-03 14:52:09 +0700

    feat(vrc25): vrc25 state impl

---
+++ core/state/vrc25.go
@@ -0,0 +1,38 @@
+// Copyright 2026 The Vic-geth Authors
+package state
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var (
+	SlotVRC25Contract = map[string]uint64{
+		"minCap":      0,
+		"tokens":      1,
+		"tokensState": 2,
+	}
+	SlotVRC25Token = map[string]uint64{
+		"balances": 0,
+		"minFee":   1,
+		"issuer":   2,
+	}
+	transferFuncHex     = common.Hex2Bytes("0xa9059cbb")
+	transferFromFuncHex = common.Hex2Bytes("0x23b872dd")
+)
+
+func GetFeeCapacity(state *StateDB, vrc25Contract common.Address, contract common.Address) *big.Int {
+	if state == nil {
+		return nil
+	}
+
+	feeCapKey := GetStorageKeyForMapping(contract.Hash(), SlotVRC25Contract["tokensState"])
+	feeCap := state.GetState(vrc25Contract, feeCapKey)
+
+	if feeCap == (common.Hash{}) {
+		return nil
+	}
+	
+	return feeCap.Big()
+}
