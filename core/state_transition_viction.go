1192af2bdfeda44eae48c8e2afe554067c92dcf0
imduchuyyy <duchuy.124dk@gmail.com>
2026-02-05 15:14:02 +0700

    fix(core): update VRC25 config access to match ChainConfig refactor

--- core/state_transition_viction.go
+++ core/state_transition_viction.go
@@ -17,14 +17,14 @@ func (st *StateTransition) vrc25BuyGas() error {
 	st.payer = st.msg.From()
 
 	// 1. Check if contract is sponsored (has fee capacity)
-	feeCap := vrc25.GetFeeCapacity(st.state, st.evm.ChainConfig().VRC25Contract, st.msg.To())
+	feeCap := vrc25.GetFeeCapacity(st.state, st.evm.ChainConfig().Viction.VRC25Contract, st.msg.To())
 	if feeCap == nil {
 		return nil // Not sponsored, proceed with standard user payment
 	}
-	chainConfig := st.evm.ChainConfig()
+	chainConfig := st.evm.ChainConfig().Viction
 
 	// 2. Calculate Gas Cost with VRC25 Gas Price
-	vrc25GasFee := new(big.Int).Mul(new(big.Int).SetUint64(st.msg.Gas()), chainConfig.VRC25GasPrice)
+	vrc25GasFee := new(big.Int).Mul(new(big.Int).SetUint64(st.msg.Gas()), (*big.Int)(chainConfig.VRC25GasPrice))
 
 	// 3. Check sufficiency
 	if feeCap.Cmp(vrc25GasFee) < 0 {
@@ -39,7 +39,7 @@ func (st *StateTransition) vrc25BuyGas() error {
 
 	// 5. Set Payer to System Contract
 	// This ensures buyGas() deducts native ETH from the system contract
-	st.gasPrice = chainConfig.VRC25GasPrice
+	st.gasPrice = (*big.Int)(chainConfig.VRC25GasPrice)
 	st.payer = chainConfig.VRC25Contract
 
 	return nil
@@ -52,7 +52,7 @@ func (st *StateTransition) isVRC25Transaction() bool {
 func (st *StateTransition) vrc25RefundGas(remaining *big.Int) {
 	addr := st.msg.To()
 	// Get current balance
-	feeCap := vrc25.GetFeeCapacity(st.state, st.evm.ChainConfig().VRC25Contract, addr)
+	feeCap := vrc25.GetFeeCapacity(st.state, st.evm.ChainConfig().Viction.VRC25Contract, addr)
 	if feeCap == nil {
 		// Should not happen if isSponsoringTransaction is true, but handle safely
 		return
@@ -61,5 +61,5 @@ func (st *StateTransition) vrc25RefundGas(remaining *big.Int) {
 	// Refund to Contract's Storage Balance
 	newFeeCap := new(big.Int).Add(feeCap, remaining)
 	feeCapKey := state.GetStorageKeyForMapping(addr.Hash(), slotTokensState)
-	st.state.SetState(st.evm.ChainConfig().VRC25Contract, feeCapKey, common.BigToHash(newFeeCap))
+	st.state.SetState(st.evm.ChainConfig().Viction.VRC25Contract, feeCapKey, common.BigToHash(newFeeCap))
 }
