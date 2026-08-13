dad3d02719da4672964d83477c26cb7fa36620d9
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2026-08-13 19:00:52 +0700

    disable snapshot signer check

--- consensus/posv/verifier.go
+++ consensus/posv/verifier.go
@@ -347,8 +347,32 @@ func (c *Posv) verifyValidators(chain consensus.ChainReader, header *types.Heade
 		}
 		snap = parentSnap
 	}
-	snapshotValidators := snap.signersNext()
-	return validateRemoteHeader(header, snapshotValidators)
+	snapshotValidators := snap.signers()
+	if err := validateRemoteHeader(header, snapshotValidators); err == nil {
+		return nil
+	} else {
+		log.Warn("[PoSV] Verify header from snapshot failed. Retry with state.", "number", number, "err", err)
+	}
+
+	// Retry to verify remote header using state.
+	var lastErr error
+	var contractValidators []common.Address
+	for gapBlockNumber = number - posvConfig.Gap; gapBlockNumber < number; gapBlockNumber++ {
+		gapHeader := chain.GetHeaderByNumber(gapBlockNumber)
+		if gapHeader == nil {
+			continue
+		}
+		validators, err := c.backend.PosvGetValidators(config, victionConfig, gapHeader, chain)
+		if err == nil && len(validators) > 0 {
+			contractValidators = validators
+			break
+		}
+		lastErr = err
+	}
+	if len(contractValidators) == 0 {
+		return lastErr
+	}
+	return validateRemoteHeader(header, contractValidators)
 }
 
 // verifySeal checks whether the signature contained in the header satisfies the
