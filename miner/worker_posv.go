792fd237d6df9ed8776f755f5d1d9a50ec357063
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2026-08-23 14:49:56 +0700

    rework blacklist mechanism

--- miner/worker_posv.go
+++ miner/worker_posv.go
@@ -161,7 +161,7 @@ func (w *worker) commitPosvTransactions(txs types.Transactions, coinbase common.
 
 // Prevent blacklisted addresses to be used in the chain.
 func (w *worker) EnforceBlacklist(tx *types.Transaction, from common.Address) (skip, pop bool) {
-	sender, receiver := misc.ValidateVictionBlackList(w.chainConfig, from, tx.To())
+	sender, receiver := misc.ValidateVictionBlackList(w.chainConfig, from, tx.To(), nil)
 	if sender {
 		return true, true
 	}
