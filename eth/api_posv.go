3ab5fa17d6f4aa0c044df70bf3f4bc80e47f12f8
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2026-08-23 14:57:18 +0700

    rpc: get validators rpc

--- eth/api_posv.go
+++ eth/api_posv.go
@@ -19,6 +19,18 @@
 
 package eth
 
+import (
+	"fmt"
+	"sort"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/sortlgc"
+	"github.com/ethereum/go-ethereum/consensus/posv"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
 type PublicPosvAPI struct {
 	e *Ethereum
 }
@@ -28,6 +40,26 @@ func NewPublicPosvAPI(e *Ethereum) *PublicPosvAPI {
 	return &PublicPosvAPI{e}
 }
 
+// Return validators for given block number.
+func (api *PublicPosvAPI) GetValidators(blockNr rpc.BlockNumber) ([]common.Address, error) {
+	chainConfig := api.e.BlockChain().Config()
+	if chainConfig.Posv == nil || chainConfig.Viction == nil {
+		return nil, core.ErrPosvRequired
+	}
+
+	var block *types.Block
+	if blockNr == rpc.LatestBlockNumber {
+		block = api.e.blockchain.CurrentBlock()
+	} else {
+		block = api.e.blockchain.GetBlockByNumber(uint64(blockNr))
+	}
+	if block == nil {
+		return nil, fmt.Errorf("block #%d not found", blockNr)
+	}
+
+	return api.e.PosvGetValidators(chainConfig, chainConfig.Viction, block.Header(), api.e.blockchain)
+}
+
 type PublicPosvDebugAPI struct {
 	e *Ethereum
 }
@@ -35,4 +67,107 @@ type PublicPosvDebugAPI struct {
 // NewPublicPosvDebugAPI creates a new PoSV consensus debug API for archive nodes.
 func NewPublicPosvDebugAPI(e *Ethereum) *PublicPosvDebugAPI {
 	return &PublicPosvDebugAPI{e}
+}
+
+type GetValidatorsResult struct {
+	BlockNumber      uint64                          `json:"number"`
+	BlockHash        string                          `json:"hash"`
+	Candidates       []*GetValidatorsCandidateDetail `json:"candidatess"`
+	SortedCandidates []*GetValidatorsCandidateDetail `json:"sortedCandidates"`
+	SortType         string                          `json:"sortType"`
+	Validators       []*GetValidatorsValidatorDetail `json:"validators"`
+	ValidatorLimit   int                             `json:"validatorLimit"`
+}
+
+type GetValidatorsCandidateDetail struct {
+	Index    int    `json:"index"`
+	Address  string `json:"address"`
+	Owner    string `json:"owner,omitempty"`
+	Capacity string `json:"capacity"`
+}
+
+type GetValidatorsValidatorDetail struct {
+	Index    int    `json:"index"`
+	Address  string `json:"address"`
+	Capacity string `json:"capacity"`
+}
+
+// Return validators for given block number with detailed steps.
+func (api *PublicPosvDebugAPI) GetValidators(blockNr rpc.BlockNumber) (*GetValidatorsResult, error) {
+	chainConfig := api.e.BlockChain().Config()
+	if chainConfig.Posv == nil || chainConfig.Viction == nil {
+		return nil, core.ErrPosvRequired
+	}
+
+	result := &GetValidatorsResult{}
+	var block *types.Block
+	if blockNr == rpc.LatestBlockNumber {
+		block = api.e.blockchain.CurrentBlock()
+	} else {
+		block = api.e.blockchain.GetBlockByNumber(uint64(blockNr))
+	}
+	if block == nil {
+		return nil, fmt.Errorf("block #%d not found", blockNr)
+	}
+	result.BlockNumber = block.NumberU64()
+	result.BlockHash = block.Hash().Hex()
+
+	statedb, err := api.e.BlockChain().StateAt(block.Root())
+	if err != nil {
+		return nil, err
+	}
+
+	candidates := statedb.VicGetCandidates(chainConfig.Viction.ValidatorContract)
+	var validators []posv.ValidatorInfo
+	result.Candidates = make([]*GetValidatorsCandidateDetail, len(candidates))
+	for i, candidate := range candidates {
+		owner, cap := statedb.VicGetValidatorInfo(chainConfig.Viction.ValidatorContract, candidate)
+		result.Candidates[i] = &GetValidatorsCandidateDetail{
+			Index:    i,
+			Address:  candidate.Hex(),
+			Owner:    owner.Hex(),
+			Capacity: cap.String(),
+		}
+		if !candidate.IsZero() {
+			validators = append(validators, posv.ValidatorInfo{Address: candidate, Capacity: cap})
+		}
+	}
+
+	if chainConfig.IsAtlas(block.Number()) {
+		result.SortType = "stable"
+		sort.SliceStable(validators, func(i, j int) bool {
+			return validators[i].Capacity.Cmp(validators[j].Capacity) >= 0
+		})
+	} else {
+		result.SortType = "legacy"
+		sortlgc.Slice(validators, func(i, j int) bool {
+			return validators[i].Capacity.Cmp(validators[j].Capacity) >= 0
+		})
+	}
+
+	result.SortedCandidates = make([]*GetValidatorsCandidateDetail, len(validators))
+	for i, v := range validators {
+		result.SortedCandidates[i] = &GetValidatorsCandidateDetail{
+			Index:    i,
+			Address:  v.Address.Hex(),
+			Capacity: v.Capacity.String(),
+		}
+	}
+
+	maxCount := int(chainConfig.Viction.ValidatorMaxCount)
+	result.ValidatorLimit = maxCount
+	final := validators
+	if len(validators) > maxCount {
+		final = validators[:maxCount]
+	}
+	result.Validators = make([]*GetValidatorsValidatorDetail, len(final))
+	for i, v := range final {
+		result.Validators[i] = &GetValidatorsValidatorDetail{
+			Index:    i,
+			Address:  v.Address.Hex(),
+			Capacity: v.Capacity.String(),
+		}
+	}
+
+	return result, nil
 }
