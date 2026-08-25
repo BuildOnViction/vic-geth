f9f55321de4b32ceb82bfab05160cf0fa11b8fcc
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2026-08-25 17:57:26 +0700

    cmd, params: change versioning format

--- params/version.go
+++ params/version.go
@@ -21,6 +21,13 @@ import (
 )
 
 const (
+	VictionMajor = 1
+	VictionMinor = 0
+	VictionPatch = 0
+	VictionMeta  = "rc1"
+)
+
+const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 10       // Minor version component of the current release
 	VersionPatch = 7        // Patch version component of the current release
@@ -29,25 +36,31 @@ const (
 
 // Version holds the textual version string.
 var Version = func() string {
-	return fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
+	return fmt.Sprintf("%d.%d.%d", VictionMajor, VictionMinor, VictionPatch)
+}()
+
+// ApiVersion holds the API version string.
+var ApiVersion = func() string {
+	return "+api." + fmt.Sprintf("%02d%02d%02d", VersionMajor, VersionMinor, VersionPatch)
 }()
 
 // VersionWithMeta holds the textual version string including the metadata.
 var VersionWithMeta = func() string {
 	v := Version
-	if VersionMeta != "" {
-		v += "-" + VersionMeta
+	if VictionMeta != "" {
+		v += "-" + VictionMeta
 	}
+	v += ApiVersion
 	return v
 }()
 
 // ArchiveVersion holds the textual version string used for Geth archives.
 // e.g. "1.8.11-dea1ce05" for stable releases, or
-//      "1.8.13-unstable-21c059b6" for unstable releases
+// "1.8.13-unstable-21c059b6" for unstable releases
 func ArchiveVersion(gitCommit string) string {
-	vsn := Version
-	if VersionMeta != "stable" {
-		vsn += "-" + VersionMeta
+	vsn := VersionWithMeta
+	if VictionMeta != "stable" {
+		vsn += "-" + VictionMeta
 	}
 	if len(gitCommit) >= 8 {
 		vsn += "-" + gitCommit[:8]
@@ -60,7 +73,7 @@ func VersionWithCommit(gitCommit, gitDate string) string {
 	if len(gitCommit) >= 8 {
 		vsn += "-" + gitCommit[:8]
 	}
-	if (VersionMeta != "stable") && (gitDate != "") {
+	if (VictionMeta != "stable") && (gitDate != "") {
 		vsn += "-" + gitDate
 	}
 	return vsn
